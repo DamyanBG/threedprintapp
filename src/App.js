f0daf2fb30a180134087e67282b8b000e17a067b
Damyan Dimitrov <d.dimitrov.1988@abv.bg>
2022-08-30 18:33:08 +0300

    add image as background

--- src/App.js
+++ src/App.js
@@ -3,20 +3,7 @@ import { Routes, Route, Link } from "react-router-dom"
 import Login from './components/Login';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-const Home = () => {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
+import Home from './components/Home';
 
 function About() {
   return (
