8bbfbaf4334aa366a6887b81d687e580b0b74e1c
Damyan Dimitrov <d.dimitrov.1988@abv.bg>
2022-08-30 17:37:37 +0300

    add react router bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,30 @@
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import inMemoryJWTManager from "../managers/inMemoryJWTManager";
 
 const Header = () => {
     return (
         <header>
-            <Navbar bg="dark" expand="lg" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">ThreeDPrintWorkShop</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
+            <Navbar bg="dark" expand="lg" variant="dark" collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>
+                            ThreeDPrintWorkShop
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                            <Nav.Link href="/login"><i className="fa-solid fa-right-to-bracket"></i>Login</Nav.Link>
+                            {
+                                !inMemoryJWTManager.getToken() && (
+                                    <Nav.Link href="/register"><i className="fa-solid fa-user-plus"></i>Register</Nav.Link>
+                                )
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
         </header>
     )
