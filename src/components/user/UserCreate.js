0219efe95151e9a8be14ffebe0f4800cbb992658
Damyan Dimitrov <d.dimitrov.1988@abv.bg>
2022-10-09 22:15:29 +0300

    add formik and implement it in register

--- src/components/user/UserCreate.js
+++ src/components/user/UserCreate.js
@@ -1,124 +1,146 @@
-import { useState } from "react"
+import { Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-const UserCreate = ({
-    fetchURL,
-    headers,
-    setJson,
-    fetchErrorMessage
-}) => {
-    const [registerInfo, setRegisterInfo] = useState({})
-
-    const fetchPostInfo = () => {
+const UserCreate = ({ fetchURL, headers, setJson, fetchErrorMessage }) => {
+    const fetchPostInfo = (postInfo) => {
         fetch(fetchURL, {
             method: "POST",
-            body: JSON.stringify(registerInfo),
-            headers: headers
+            body: JSON.stringify(postInfo),
+            headers: headers,
         })
-            .then(resp => resp.json())
-            .then(json => {
-                setJson(json)
+            .then((resp) => resp.json())
+            .then((json) => {
+                setJson(json);
             })
-            .catch(() => alert(fetchErrorMessage))
-    }
+            .catch(() => alert(fetchErrorMessage));
+    };
 
-    const handleInfoChange = (e) => {
-        setRegisterInfo({
-            ...registerInfo,
-            [e.target.name]: e.target.value
-        })
-    }
+    const createSchema = Yup.object().shape({
+        first_name: Yup.string()
+            .min(2, "Too Short!")
+            .max(50, "Too Long!")
+            .required("Required!"),
+        last_name: Yup.string()
+            .min(2, "Too Short!")
+            .max(50, "Too Long!")
+            .required("Required!"),
+        phone: Yup.string()
+            .min(8, "Too Short!")
+            .max(20, "Too Long!")
+            .required("Required!"),
+        email: Yup.string()
+            .email("Invalid email")
+            .required("Required!"),
+        password: Yup.string()
+            .min(5, "Too Short!")
+            .max(50, "Too Long!")
+            .required("Required!")
+    });
 
-    const handleSubmitInfo = (e) => {
-        e.preventDefault()
-        fetchPostInfo()
-    }
+    const handleSubmitInfo = (values) => {
+        fetchPostInfo(values);
+    };
 
     return (
         <>
-            <h1>Register</h1>
-            <form
-                style={{
-                    background: "lightblue", 
-                    padding: "10px",
-                    marginLeft: "30%",
-                    marginRight: "30%",
-                    borderRadius: "6px"
-                }}
-                onSubmit={handleSubmitInfo}
+            <h1 className="text-center">Register</h1>
+            <Formik 
+                initialValues={{}}
+                validationSchema={createSchema}
+                onSubmit={(values) => handleSubmitInfo(values)}
             >
-                <div>
-                    <label>First name</label>
-                    <input 
-                        type="text" 
-                        name="first_name"
+                {(formik) => (
+                    <form
                         style={{
-                            marginLeft: "5px",
-                            borderRadius: "5px"
+                            background: "lightblue",
+                            padding: "10px",
+                            marginLeft: "30%",
+                            marginRight: "30%",
+                            borderRadius: "6px",
                         }}
-                        value={registerInfo.first_name || ""}
-                        onChange={handleInfoChange}
-                    />
-                </div>
-                <div>
-                    <label>Last name</label>
-                    <input 
-                        type="text" 
-                        name="last_name"
-                        style={{
-                            marginLeft: "5px",
-                            borderRadius: "5px"
-                        }}
-                        value={registerInfo.last_name || ""}
-                        onChange={handleInfoChange}
-                    />
-                </div>
-                <div>
-                    <label>Phone</label>
-                    <input 
-                        type="number" 
-                        name="phone"
-                        style={{
-                            marginLeft: "5px",
-                            borderRadius: "5px"
-                        }}
-                        value={registerInfo.phone || ""}
-                        onChange={handleInfoChange}
-                    />
-                </div>
-                <div>
-                    <label>E-mail</label>
-                    <input 
-                        type="email" 
-                        name="email"
-                        style={{
-                            marginLeft: "5px",
-                            borderRadius: "5px"
-                        }}
-                        value={registerInfo.email || ""}
-                        onChange={handleInfoChange}
-                    />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input 
-                        type="text" 
-                        name="password"
-                        style={{
-                            marginLeft: "5px",
-                            borderRadius: "5px"
-                        }}
-                        value={registerInfo.password || ""}
-                        onChange={handleInfoChange}
-                    />
-                </div>
-                <button
-                    type="submit"
-                >
-                    Submit
-                </button>
-            </form>
+                        onSubmit={formik.onSubmit}
+                    >
+                        <div>
+                            <label>First name</label>
+                            <input
+                                type="text"
+                                name="first_name"
+                                style={{
+                                    marginLeft: "5px",
+                                    borderRadius: "5px",
+                                }}
+                                value={formik.values.first_name || ""}
+                                onBlur={formik.handleBlur}
+                                onChange={formik.handleChange}
+                            />
+                            <ErrorMessage name="first_name"/>
+                        </div>
+                        <div>
+                            <label>Last name</label>
+                            <input
+                                type="text"
+                                name="last_name"
+                                style={{
+                                    marginLeft: "5px",
+                                    borderRadius: "5px",
+                                }}
+                                value={formik.values.last_name || ""}
+                                onBlur={formik.handleBlur}
+                                onChange={formik.handleChange}
+                            />
+                            <ErrorMessage name="last_name"/>
+                        </div>
+                        <div>
+                            <label>Phone</label>
+                            <input
+                                type="number"
+                                name="phone"
+                                style={{
+                                    marginLeft: "5px",
+                                    borderRadius: "5px",
+                                }}
+                                value={formik.values.phone || ""}
+                                onBlur={formik.handleBlur}
+                                onChange={formik.handleChange}
+                            />
+                            <ErrorMessage name="phone"/>
+                        </div>
+                        <div>
+                            <label>E-mail</label>
+                            <input
+                                type="email"
+                                name="email"
+                                style={{
+                                    marginLeft: "5px",
+                                    borderRadius: "5px",
+                                }}
+                                value={formik.values.email || ""}
+                                onBlur={formik.handleBlur}
+                                onChange={formik.handleChange}
+                            />
+                            <ErrorMessage name="email"/>
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                name="password"
+                                style={{
+                                    marginLeft: "5px",
+                                    borderRadius: "5px",
+                                }}
+                                value={formik.values.password || ""}
+                                onBlur={formik.handleBlur}
+                                onChange={formik.handleChange}
+                            />
+                            <ErrorMessage name="password"/>
+                        </div>
+                        <button type="submit">Submit</button>
+                    </form>
+                )}
+            </Formik>
         </>
-    )
-}
+    );
+};
 
 export default UserCreate;
